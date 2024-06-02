5b1d1cae389f4524567eec52a0d26fb75befce17
Sukkoth <suukootj@gmail.com>
2024-06-02 10:28:15 +0300

    stale time for repos, there won't be that much demanding change to fetch over and over

--- src/react-query/queries/index.ts
+++ src/react-query/queries/index.ts
@@ -5,5 +5,6 @@ export function useGetRepos() {
   return useQuery({
     queryKey: ["repos"],
     queryFn: GITHUB_SERVICE.GET_REPOS,
+    staleTime: 4_000_000, // for 1 hour, there won't be much data to be changed
   });
 }
