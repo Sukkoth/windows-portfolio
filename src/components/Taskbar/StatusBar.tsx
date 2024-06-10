dbe159da2fd36bc274d8c9ac943ca121ede74e74
Sukkoth <suukootj@gmail.com>
2024-06-10 15:29:12 +0300

    iPhone test#3

--- src/components/Taskbar/StatusBar.tsx
+++ src/components/Taskbar/StatusBar.tsx
@@ -7,7 +7,7 @@ function StatusBar() {
     <div className='items-center gap-2 hidden lg:flex'>
       <div className='flex items-center gap-2 relative before:content-[" "] before:-left-4 w-full h-full before:-right-4 before:-top-3 before:-bottom-3 before:bg-hover-color before:absolute before:-z-10 before:rounded-lg before:hidden hover:before:block cursor-pointer  before:shadow-inner before:shadow-stone-700'>
         <Network />
-        <Battery />
+        {/* <Battery /> */}
       </div>
       <Time />
     </div>
