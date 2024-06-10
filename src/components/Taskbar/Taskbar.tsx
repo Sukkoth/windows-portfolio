4f31190044602564172f748f8a7c621e210395f0
Sukkoth <suukootj@gmail.com>
2024-06-10 15:08:09 +0300

    test iPhone error

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -1,12 +1,11 @@
 import ItemsList from "./ItemsList";
-import StatusBar from "./StatusBar";
 
 function TaskBar() {
   return (
     <div className='absolute flex justify-between items-center left-0 right-0 py-[8px] bg-[#222224f1] border-t border-t-stone-700 z-[99999] bottom-0 inset-x-0'>
       <div className='hidden lg:block w-40'></div>
       <ItemsList />
-      <StatusBar />
+      {/* <StatusBar /> */}
     </div>
   );
 }
