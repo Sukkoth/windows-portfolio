1cb8e6073608239769675ff7dca234b0e37f99f2
Sukkoth <suukootj@gmail.com>
2024-06-10 15:50:09 +0300

    back to default with status bar

--- src/components/Taskbar/StatusBar.tsx
+++ src/components/Taskbar/StatusBar.tsx
@@ -1,13 +1,13 @@
-// import Battery from "./Battery";
-// import Network from "./Network";
+import Battery from "./Battery";
+import Network from "./Network";
 import Time from "./Time";
 
 function StatusBar() {
   return (
     <div className='items-center gap-2 hidden lg:flex'>
       <div className='flex items-center gap-2 relative before:content-[" "] before:-left-4 w-full h-full before:-right-4 before:-top-3 before:-bottom-3 before:bg-hover-color before:absolute before:-z-10 before:rounded-lg before:hidden hover:before:block cursor-pointer  before:shadow-inner before:shadow-stone-700'>
-        {/* <Network /> */}
-        {/* <Battery /> */}
+        <Network />
+        <Battery />
       </div>
       <Time />
     </div>
