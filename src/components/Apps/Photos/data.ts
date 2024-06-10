f7bf2a667bcc4044b8b19c93cad02bd184a0f031
Sukkoth <suukootj@gmail.com>
2024-06-10 09:51:03 +0300

    images path

--- src/components/Apps/Photos/data.ts
+++ src/components/Apps/Photos/data.ts
@@ -2,31 +2,31 @@ export const pictures = [
   {
     id: 1,
     projectId: 2,
-    path: "/public/assets/homepage.jpg",
+    path: "/assets/homepage.jpg",
   },
   {
     id: 1,
     projectId: 2,
-    path: "/public/assets/homepage0.png",
+    path: "/assets/homepage0.png",
   },
   {
     id: 1,
     projectId: 2,
-    path: "/public/assets/homepage2.jpg",
+    path: "/assets/homepage2.jpg",
   },
   {
     id: 1,
     projectId: 2,
-    path: "/public/assets/homepage3.jpg",
+    path: "/assets/homepage3.jpg",
   },
   {
     id: 1,
     projectId: 2,
-    path: "/public/assets/homepage4.png",
+    path: "/assets/homepage4.png",
   },
   {
     id: 1,
     projectId: 2,
-    path: "/public/assets/homepage5.png",
+    path: "/assets/homepage5.png",
   },
 ];
