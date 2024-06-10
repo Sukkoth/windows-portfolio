e0d297fdac8dd8940ba1184e2dc55617714f64c8
Sukkoth <suukootj@gmail.com>
2024-06-10 14:55:46 +0300

    more project images

--- src/components/Apps/Photos/index.tsx
+++ src/components/Apps/Photos/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { pictures as PICTURES_DATA } from "./data";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Photos() {
+  const navigate = useNavigate();
   //get project specific images,
   //set the image id to the index
   const {
-    state: { projectId, pictureId, pictureIndex },
+    state: { projectId, pictureIndex },
   } = useLocation();
 
   const projectToShow = PICTURES_DATA.filter((picture) => {
@@ -22,15 +23,6 @@ function Photos() {
   const handleImageLoad = () => {
     setLoading(false);
   };
-
-  console.log({
-    projectId,
-    pictureId,
-    pictureIndex,
-    projectToShow,
-    PICTURES_DATA,
-    showing,
-  });
 
   function handleNav(increment: boolean = false) {
     setLoading(true); //set loading true when image resets,
@@ -51,35 +43,49 @@ function Photos() {
   return (
     <div className='overflow-hidden h-full bg-stone-900'>
       <div className='outline-none h-full object-cover flex flex-col items-center justify-center'>
-        <div className='w-full lg:w-[80%] h-[80%] relative'>
-          {loading && (
-            <div className='absolute inset-0 flex bg-stone-900 items-center justify-center z-10'>
+        {projectToShow.length > 0 ? (
+          <>
+            <div className='w-full lg:w-[80%] h-[80%] relative'>
+              {loading && (
+                <div className='absolute inset-0 flex bg-stone-900 items-center justify-center z-10'>
+                  <img
+                    className='animate-spin size-10 brightness-75'
+                    src='https://img.icons8.com/?size=100&id=0BQGp1zaYp5N&format=png&color=ffffff'
+                  />
+                </div>
+              )}
               <img
-                className='animate-spin size-10 brightness-75'
-                src='https://img.icons8.com/?size=100&id=0BQGp1zaYp5N&format=png&color=ffffff'
+                onLoad={handleImageLoad}
+                src={projectToShow[showing].imagePath}
+                alt='image'
+                className='object-contain size-full'
               />
             </div>
-          )}
-          <img
-            onLoad={handleImageLoad}
-            src={projectToShow[showing].imagePath}
-            alt='image'
-            className='object-contain size-full'
-          />
-        </div>
-        <div className='mt-3 w-[50%] p-2 flex justify-between items-center'>
-          <MenuItem
-            onClick={() => handleNav()}
-            imageUrl='https://img.icons8.com/?size=100&id=1806&format=png&color=ffffff'
-          />
-          <h1 className='text-xl'>
-            {showing + 1} | {projectToShow.length}
-          </h1>
-          <MenuItem
-            onClick={() => handleNav(true)}
-            imageUrl='https://img.icons8.com/?size=100&id=AgsBKswRnV1i&format=png&color=ffffff'
-          />
-        </div>
+            <div className='mt-3 w-[50%] p-2 flex justify-between items-center'>
+              <MenuItem
+                onClick={() => handleNav()}
+                imageUrl='https://img.icons8.com/?size=100&id=1806&format=png&color=ffffff'
+              />
+              <h1 className='text-xl'>
+                {showing + 1} | {projectToShow.length}
+              </h1>
+              <MenuItem
+                onClick={() => handleNav(true)}
+                imageUrl='https://img.icons8.com/?size=100&id=AgsBKswRnV1i&format=png&color=ffffff'
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <h1>No images found for the project . . .</h1>
+            <button
+              className='px-7 py-2 rounded-xl bg-purple-500 mt-3 hover:bg-purple-600'
+              onClick={() => navigate(-1)}
+            >
+              Go back
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
