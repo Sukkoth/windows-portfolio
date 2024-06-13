f4f814bcb6b66b113469341cec4d7a8c0226b372
Sukkoth <suukootj@gmail.com>
2024-06-13 10:51:54 +0300

    fix z index that hid desktop icons

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -13,7 +13,7 @@ function Day() {
   const today = new Date().getDay();
 
   return (
-    <div className='absolute inset-x-0 top-36 text-center text-4xl sm:text-4xl md:text-6xl xl:text-8xl font-anurati uppercase tracking-widest'>
+    <div className='absolute inset-x-0 top-36 text-center text-4xl sm:text-4xl md:text-6xl xl:text-8xl font-anurati uppercase tracking-widest -z-10'>
       <h1> {weekdays[today]}</h1>
       <h3 className='mt-5 text-2xl sm:text-3xl md:text-5xl xl:text-7xl font-bold font-time-font'>
         {formatTime()}
