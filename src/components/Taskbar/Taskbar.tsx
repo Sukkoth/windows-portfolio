915ce0550bfa55058ad5de4a768b91f2afb1cd4b
Sukkoth <suukootj@gmail.com>
2024-06-13 15:18:22 +0300

    taskbar transparency

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -3,7 +3,7 @@ import StatusBar from "./StatusBar";
 
 function TaskBar() {
   return (
-    <div className='absolute flex justify-between items-center left-0 right-0 bg-[#222224f1] border-t border-t-stone-700 z-[99999] bottom-0 inset-x-0 h-[3.5rem]'>
+    <div className='absolute flex justify-between items-center left-0 right-0 bg-[#222224fb] border-t border-t-stone-700  z-[99999] bottom-0 inset-x-0 h-[3.5rem]'>
       <div className='hidden lg:block w-40'></div>
       <ItemsList />
       <StatusBar />
