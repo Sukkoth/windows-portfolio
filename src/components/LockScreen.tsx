c00340c526bbb3ab5effc152846109f3ae1efb01
Sukkoth <suukootj@gmail.com>
2024-06-17 13:38:02 +0300

    refactor lockscreen

--- src/components/LockScreen.tsx
+++ src/components/LockScreen.tsx
@@ -1,8 +1,4 @@
-import { Position } from "@/services/weather/types";
-import { formatTime, getDayName } from "@/utils/date";
-import DayCard from "@/components/Apps/Weather/DayCard";
-import { useEffect, useState } from "react";
-import { useGetWeatherData } from "@/react-query/queries";
+import { formatTime } from "@/utils/date";
 import { useTab } from "@/Provider/TabProvider";
 
 function LockScreen() {
@@ -13,49 +9,23 @@ function LockScreen() {
     day: "numeric",
   });
 
-  const [location, setLocation] = useState<Position | null>(null);
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLocation({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      });
-    });
-  }, []);
-
-  const { data: weather } = useGetWeatherData(location);
-
   return (
     <div
       onDoubleClick={() => toggleAsleep(false)}
-      className={`backdrop-blur-xl h-[100dvh] w-full flex items-center flex-col select-none text-white `}
+      className={`backdrop-blur-lg h-[100dvh] flex items-center flex-col select-none text-white relative`}
     >
-      <h1 className='text-[10rem] font-medium tracking-wide mt-[5rem]'>
+      <h1 className='text-[6rem] md:text-[10rem] font-medium tracking-wide mt-[2rem] md:mt-[3rem]'>
         {formatTime()}
       </h1>
-      <p className='text-[2rem]'>{formattedDate}</p>
+      <p className='text-xl md:text-3xl md:-mt-8 -mt-3'>{formattedDate}</p>
       <img
-        className='my-5'
+        className='my-3 size-10 md:size-20'
         src='https://img.icons8.com/?size=100&id=1F5Aok9b4nxz&format=png&color=000000'
         alt=''
       />
-      <p className='font-mono'>🤩 Double Click to unlock</p>
-      {weather && (
-        <div className='absolute bottom-20'>
-          <div className='flex gap-5 mt-14 items-center justify-center'>
-            {weather.forecast.forecastday.map((day, index) => (
-              <DayCard
-                top={getDayName(new Date(day.date).getDay(), true)}
-                middle={day.day.avgtemp_c.toString()}
-                bottom={day.day.condition.text}
-                key={index}
-                icon={day.day.condition.icon}
-              />
-            ))}
-          </div>
-        </div>
-      )}
+      <p className='font-extralight tracking-wider text-sm'>
+        🤩 Double Click to unlock
+      </p>
     </div>
   );
 }
