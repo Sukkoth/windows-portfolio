df8406f4ebab1e14705cf94f5447aa8ab16fdf5d
Sukkoth <99675293+Sukkoth@users.noreply.github.com>
2024-06-20 17:41:27 +0300

    Fix active inversion

--- src/components/QuickSettings/index.tsx
+++ src/components/QuickSettings/index.tsx
@@ -9,7 +9,7 @@ function QuickSettings({ active = false }: Props) {
   return (
     <div
       className={`absolute duration-300  ${
-        active ? "-bottom-96" : "bottom-16"
+        !active ? "-bottom-96" : "bottom-16"
       } right-5 border border-stone-600 bg-stone-700/50 backdrop-blur-[100px] rounded-md z-[9999] text-sm font-extralight w-80 overflow-hidden`}
     >
       <div className='p-5'>
