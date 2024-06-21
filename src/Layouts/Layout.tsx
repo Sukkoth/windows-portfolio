7489f76f54f42769569bfcfb96ee432cae807a3d
Sukkoth <suukootj@gmail.com>
2024-06-21 10:10:33 +0300

    unused feature remove

--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -1,6 +1,6 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import TaskBar from "@/components/Taskbar/Taskbar";
-import { Suspense, lazy, useEffect, useMemo, useState } from "react";
+import { Suspense, lazy, useCallback, useState } from "react";
 import { useTab } from "@/Provider/TabProvider";
 import Main from "@/components/Suspense/Main";
 import Terminal from "@/components/Apps/Terminal";
@@ -11,47 +11,10 @@ const LockScreen = lazy(() => import("@/components/LockScreen"));
 function Layout() {
   const { asleep, showTerminal } = useTab();
   const [showQuickSettings, setShowQuickSettings] = useState(false);
-  const [showPrev, setShowPrev] = useState(false);
-  const [history, setHistory] = useState<string[]>([]);
 
-  const keysPressed: { [key: string]: boolean } = useMemo(() => {
-    return {};
+  const toggleQuickSettings = useCallback(() => {
+    setShowQuickSettings((prev) => !prev);
   }, []);
-
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (history.some((item) => item === pathname)) return;
-    setHistory((prev) => [...prev, pathname]);
-  }, [pathname, history]);
-
-  useEffect(() => {
-    function keyDownFunc(event: KeyboardEvent) {
-      keysPressed[event.key] = true;
-
-      if (event.key === "Tab") {
-        event.preventDefault();
-      }
-
-      if (keysPressed["Tab"] && event.key == "Alt") {
-        setShowPrev(true);
-      }
-    }
-
-    function keyUpFunc(event: KeyboardEvent) {
-      delete keysPressed[event.key];
-      setShowPrev(false);
-    }
-
-    document.addEventListener("keydown", keyDownFunc);
-    document.addEventListener("keyup", keyUpFunc);
-
-    return () => {
-      document.removeEventListener("keydown", keyDownFunc);
-      document.removeEventListener("keyup", keyUpFunc);
-    };
-  }, [keysPressed]);
 
   if (asleep) {
     return (
@@ -65,26 +28,8 @@ function Layout() {
     <main className='text-white h-[100dvh] text-sm overflow-hidden relative'>
       <div className='h-[94.8dvh]'>
         <Outlet />
-        {showPrev && (
-          <div className='inset-0 z-[99999] absolute flex overflow-scroll gap-10 items-center justify-center'>
-            {[...new Set(history)].map((item) => (
-              <div
-                className='min-w-[25rem] min-h-[15rem] bg-stone-800 ring ring-white ring-offset-4 ring-offset-pink-400 rounded-2xl flex items-center justify-center hover:scale-105 duration-300'
-                key={item}
-                onClick={() => {
-                  setShowPrev(false);
-                  navigate(item);
-                }}
-              >
-                <p>{item}</p>
-              </div>
-            ))}
-          </div>
-        )}
       </div>
-      <TaskBar
-        toggleQuickSettings={() => setShowQuickSettings((prev) => !prev)}
-      />
+      <TaskBar toggleQuickSettings={toggleQuickSettings} />
       <QuickSettings active={showQuickSettings} />
       {showTerminal && <Terminal />}
     </main>
