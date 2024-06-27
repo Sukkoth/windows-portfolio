b1482e9314f58996204ca2a7a54c14348d37d584
Sukkoth <suukootj@gmail.com>
2024-06-27 10:08:34 +0300

    added hidden feature to show desktop :)

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import ItemsList from "./ItemsList";
 import StatusBar from "./StatusBar";
+import { useTab } from "@/Provider/TabProvider";
 
 type Props = {
   toggleQuickSettings: () => void;
@@ -10,6 +12,10 @@ type Props = {
 };
 
 function TaskBar(props: Props) {
+  const { toggleTerminal } = useTab();
+
+  const navigate = useNavigate();
+
   return (
     <div className='absolute flex items-center left-0 right-0 bg-[#222224fb] border-t border-t-stone-700 z-[99999] bottom-0 inset-x-0 h-[3.5rem]'>
       <div className='hidden lg:block w-[15%]'></div>
@@ -19,6 +25,14 @@ function TaskBar(props: Props) {
       <div className='w-fit md:flex ms-10 items-center gap-2 hidden'>
         <StatusBar {...props} />
       </div>
+      <div
+        title='Show Desktop'
+        className='absolute top-0 bottom-0 w-10 -right-1 bg-transparent'
+        onClick={() => {
+          toggleTerminal(false);
+          navigate("/");
+        }}
+      ></div>
     </div>
   );
 }
